30ce7e2a2d0aa9e9bcf7cc2879056586e87e1b8c
“zmjios” <852838218@qq.com>
2015-05-08 23:45:17 +0800

    Initial Commit

---
+++ TCRefreshControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCRefreshControl
+//
+//  Created by 曾明剑 on 15/5/8.
+//  Copyright (c) 2015年 zmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCRefreshControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TCRefreshControl
+//
+//  Created by 曾明剑 on 15/5/8.
+//  Copyright (c) 2015年 zmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
